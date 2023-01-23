47e3916cfcde37a1d9edffabaf8fa44b17d2303c
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-23 17:35:28 +0100

    testingPhase

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,8 @@ let labelLuftqualität = document.querySelector("#labelLuftqualität");
 let outputLevelConcern = document.querySelector("#outputLevelConcern");
 let outputLevelEffect = document.querySelector("#outputLevelEffect");
 
+let outputBody = document.querySelector("body");
+
 
 // Eventlistener
 luftqualität.addEventListener("input", checkAirQuality);
@@ -36,12 +38,43 @@ console.log("function und EventListener aufgerufen");
 // Anzeige des Range Wertes         
 labelLuftqualität.innerHTML = ("Luftqualität: " +  luftqualität.value);
 
+let vergleich = luftqualität.value;
+let ergebnisConcern = "???"; 
+let ergebnisEffect = "???"; 
+let ergebnisBackgroundColorClass = "???"; 
+
+// if Vergleich
+if(vergleich >=0 && vergleich <= 50){ 
+    ergebnisConcern =  "Good";
+    ergebnisEffect = "Little or no risk";
+    ergebnisBackgroundColorClass = "green";
+}
+else if (vergleich >50 && vergleich <= 100){
+    ergebnisConcern = "Moderate";
+    ergebnisEffect = "Acceptable quality";
+    ergebnisBackgroundColorClass = "yellow";
+}
+else if (vergleich >100 && vergleich <= 150){
+    ergebnisConcern = "Unhealthy for sensitive groups";
+    ergebnisEffect = "Generable public not likely affected";
+    ergebnisBackgroundColorClass = "red";
+}
+
 
 
 
 // output Text concern
+outputLevelConcern.innerHTML = ("Level of healt: " + ergebnisConcern);
+
 // Output Text effect
+outputLevelEffect.innerHTML = ("Level of health effect: " + ergebnisEffect);
 
 // Output BackgroundColor  über js_classen
+outputBody.style.backgroundColor = ergebnisBackgroundColorClass;
+
+
+// geht nicht was anstatt .add ???
+// outputBody.classList.replace(ergebnisBackgroundColorClass);
+
 
 }
