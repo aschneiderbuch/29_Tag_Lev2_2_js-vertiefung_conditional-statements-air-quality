a4260a06383657186986eb432ed724d9cbea0f4b
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-23 15:15:06 +0100

    devStart

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
 console.log("läuft?");
+
+/* 
+Frage
+
+    <=50  = Good + Little or no risk
+    >50   = Moderate + Acceptable quality
+    >100 && <=150     = Unhealthy for sensitive groups + generable public not likely affected
+    
+    Range Slider
+                Value vom Slider Abfragen 
+
+                function checkAirQuality()
+
+    Background Color wechsel 
+*/
+
+
